5721da3a5b70cb391225c8e11882806952bde65f
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-04-02 08:47:00 +0200

    Add files via upload

--- performance.r
+++ performance.r
@@ -1,7 +1,7 @@
 # LIBRARIES ----
 library(tidyverse)
 
-## DATA ----
+# DATA ----
 data <- standings |> 
   left_join(teams, by = "Team") |> 
   mutate(Spiele = W - L,
@@ -29,14 +29,14 @@ ggplot(data) +
   scale_x_continuous(name = "Punktedifferenz", breaks = function(x) seq(from = floor(x[1]/30)*30, to = ceiling(x[2]/30)*30, by = 30)) +
   scale_y_continuous(name = "Differenz (Siege - Niederlagen)", breaks = function(x) seq(from = floor(x[1]/2)*2, to = ceiling(x[2]/2)*2, by = 2),
                      minor_breaks = function(x) seq(from = floor(x[1]), to = ceiling(x[2]), by = 1)) +
-  labs(title = "Vergleich der Teams", 
+  labs(title = "Tabelle grafisch", 
        subtitle = "2025 FLJ U13 Division I") +
   theme(plot.title = element_text(size = 16),
         plot.subtitle = element_text(size = 13),
         panel.background = element_rect(fill = "lightgreen"),
         panel.grid = element_line(color = "white", linewidth = 1.5)) -> p
 
-windows(16,16)
+windows(16, 16)
 plot(p)
 
 rm(data, p)
---
+++ win ratio.r
@@ -0,0 +1,34 @@
+# LIBRARIES ----
+library(tidyverse)
+
+# DATA ----
+data <- standings |> 
+  left_join(teams, by = "Team") |> 
+  mutate(EWR = 1 / (1 + (PG / PF) ** 2.37),
+         Delta = (as.numeric(Pct) - EWR))
+
+# PLOT ----
+ggplot(data) +
+  aes(x = EWR, y = Pct) +
+  geom_abline(slope = 1, intercept = 0, linetype = "solid") +
+  geom_point(mapping = aes(color = Delta * 10), size = 12) +
+  ggimage::geom_image(inherit.aes = TRUE, image = "fb_32.png", size = .03) +
+  ggrepel::geom_label_repel(mapping = aes(label = Kurz), segment.linetype = 2,
+                            box.padding = 1.25, label.padding = unit(.2, "lines"), point.padding = .25,
+                            max.overlaps = 8, size = 3) +
+  scale_x_continuous(name = "Wahre Win Ratio") +
+  scale_y_continuous(name = "Erwartete Win Ratio (Pythagoreisch)") +
+  scale_color_distiller(name = "Overwinning\npro 10 Spiele", palette = "RdBu", direction = -1, values = scales::rescale(c(range(data$Delta), 0)[c(1, 3, 2)])) +
+  labs(title = "Wahre und erwartete Win Ratio",
+       subtitle = "2025 FLJ U13 Division I") +
+  coord_fixed(xlim = c(0, 1), ylim = c(0, 1)) +
+  theme(plot.title = element_text(size = 16),
+        plot.subtitle = element_text(size = 13),
+        panel.background = element_rect(fill = "lightgreen"),
+        panel.grid = element_line(color = "white", linewidth = 1.5)) -> p
+
+windows(16, 16)
+plot(p)
+
+rm(data, p)
+
