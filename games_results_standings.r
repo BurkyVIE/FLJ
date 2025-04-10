7de7bce14cab043901f28be288f688ac32386824
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-04-10 09:30:34 +0200

    Add files via upload

--- games_results_standings.r
+++ games_results_standings.r
@@ -17,7 +17,7 @@ data_raw <- dir()[str_detect(dir(),pattern = "FLJ.*\\.txt")] |>
                                          P_H = col_integer(),
                                          P_G = col_integer()))))
 
-data_raw <- data_raw |> select(-(file:Liga)) |> unnest_longer(Data) |> unpack(Data)
+data <- data_raw |> select(-(file:Liga)) |> unnest_longer(Data) |> unpack(Data)
 
 # data_raw <- read_csv("FLJ 2025 U13 D1.txt", lazy = FALSE,
 #                      col_types = cols(Datum = col_date(),
@@ -33,16 +33,16 @@ data_raw <- data_raw |> select(-(file:Liga)) |> unnest_longer(Data) |> unpack(Da
 team_suche <- c("Angels", "Dragons", "Pannonian", "Giants", "Indians", "Raiders", "Spartans", "Steelsharks",
                "Badgers", "Bastards", "Carinthian", "Invaders", "Knights", "Legionaries", "Vikings", "Vipers")
 team_kurz <- c("Angels", "Dragons", "P.Eagles", "Giants", "Indians", "Raiders", "Spartans", "Steelsharks",
-               "Badgers", "Bastards/Hawks", "C.Eagles", "Invaders", "Knights", "Legionaries", "Vikings", "Vipers")
+               "Badgers", "Bastards", "C.Eagles", "Invaders", "Knights", "Legionaries", "Vikings", "Vipers")
 # teams <- unique(c(data_raw$Heim, data_raw$Gast))
-teams <- data_raw |> select(Saison, Stufe, Div, Heim) |> unique() |> rename(Team = Heim)
+teams <- data |> select(Saison, Stufe, Div, Heim) |> unique() |> rename(Team = Heim)
 he <- map_int(team_suche, ~which(str_detect(teams$Team, .)))
 teams <- tibble(teams[he,], Kurz = team_kurz)
 rm(team_suche, team_kurz, he)
 
 # OUTPUT ----
 ## Games ----
-games <- filter(data_raw, !is.na(P_H) | !is.na(P_G))
+games <- filter(data, !is.na(P_H) | !is.na(P_G))
 
 ## Results ----
 results <- bind_rows(rename(games, Team = Heim, Gegner = Gast, PF = P_H, PG = P_G),
