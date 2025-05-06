a1d184eb39a68c333e5b759ef120dbf23c1ab12b
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-05-06 20:42:14 +0200

    Delete statistik.r

--- statistik.r
+++
@@ -1,4 +0,0 @@
-kader <- read_csv("FLJ 2025 U13 Dark Angels Kader.txt", lazy = FALSE, col_types = cols(No = col_integer(), Name = col_character(), Jahrgang = col_integer()))
-
-source("FLJ 2025 U13 Dark Angels Statistik.txt")
-statistik <- statistik |> unnest(Data) |> left_join(kader, by = "No", relationship = "many-to-one") |> left_join(games, by = "SpielID")
