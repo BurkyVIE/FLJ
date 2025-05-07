0da96ac1bec1439315bc94175902965466c116e9
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-05-07 08:52:25 +0200

    Add files via upload

--- stats.r
+++ stats.r
@@ -3,12 +3,18 @@ kader <- read_csv("FLJ 2025 U13 Dark Angels Kader.txt", lazy = FALSE, col_types 
 source("FLJ 2025 U13 Dark Angels Statistik.txt")
 stats <- unnest(stats, Data) |>
   add_column(Active = TRUE) |>
-  unnest_wider(Detail)
+  unnest_wider(Detail, )
 stats <- stats |> left_join(kader, by = "No", relationship = "many-to-one") |> left_join(games, by = "SpielID")
 
 stats |>
   filter(Gruppe == "Def") |> 
   group_by(No, Name) |>
-  summarise(Pulls = sum(Pull), Games = n_distinct(SpielID), .groups = "drop") |> 
+  summarise(Games = n_distinct(SpielID),
+            Pulls = sum(Pull, na.rm = TRUE),
+            Ints = sum(Int, na.rm = TRUE),
+            Pick6s = sum(Pick6, na.rm = TRUE),
+            Sacks = sum(Sack, na.rm = TRUE),
+            Safeties = sum(Safety, na.rm = TRUE),
+            .groups = "drop") |> 
   mutate(PullspG = Pulls/Games) |> 
   arrange(desc(PullspG), No)
